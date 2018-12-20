598901ae49be8f1e9c5d1f78a21f75475ddfd10e
KrystianZakrys <krystian.zakrys@gmail.com>
2018-12-20 23:16:36 +0100

    Dodanie paru metod.

--- LBG/LBG.py
+++ LBG/LBG.py
@@ -1,10 +1,10 @@
 #20.12.2018
-#TODO: wczytywanie. Dane oddzielane spacją, przecinek na kropkę, czasem jest zapis [,2] co znacza 0.2 ostatnia kolumna to zawsze klasa. 
-#TODO: Serializacja
+#TODO: Poprawić wczytywanie dla arythmii
 
 #21.12.2018
 #TODO: Algorytm 
 #TODO: testy i raport
+
 import csv
 import sys
 import os
@@ -13,7 +13,6 @@ class Instance(object):
     index = 0
     className = 'init'
     args = []
-    args_names = []
 
     def __init__(self, index, className, args):
         self.index = index  
@@ -30,26 +29,64 @@ class Instance(object):
         for arg in self.args:
             argsString += str(arg) + ' '
 
+
+        commentsString = ''
+        for comment in self.comments:
+            commentsString += comment
+
+        return 'Index: '+str(self.index)+'\nClassName: '+self.className+'\nArgs: '+argsString
+
+class Instances(object):
+    fileName = ''
+    args_names = []
+    comments = []
+    instances = []
+
+    def __init__(self):
+        fileName = 'BRAK PLIKU'
+
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def __init__(self,fileName, args_Names, comments, instances):
+        self.fileName = fileName
+        self.args_names = args_Names
+        self.comments = comments
+        self.instances = instances
+
+    def toString(self):
         argsNamesString = ''
         for argName in self.args_names:
-            argsNamesString += argName +' '
+            argsNamesString += argName.strip()+', '
 
-        return 'Index: '+str(self.index)+'\nClassName: '+self.className+'\nArgs: '+argsString+'\nArgs_Names: '+argsNamesString
+        commentsString = ''
+        for comment in self.comments:
+            commentsString += comment
 
-instances = []
+        return '\nFileName: ' + self.fileName +'\nArgs Names: '+ argsNamesString +'\nInstances Count: '+str(len(self.instances))+'\nComments: '+commentsString 
+
+
+    def showInstances(self):
+        for instance in self.instances:
+            instanceString = ''
+            for arg in instance.args:
+                instanceString += str(arg) +'\t'
+            print(instanceString)
 
 def load_data(path):
     f = open(path,'r')
     f_content = f.readlines()
     index = 0
+    comments = []
+    instances = []
+
     for line in f_content:
         if line[0] == '#' or line.isspace():
             #to linia komentarza więc ignoruj ją albo dodaj do jakiego obiektu narazie olać
-            print(' ')
+            comments.append(line)
         else:
             if index == 0:
                 #ta linia to nazwy kolumn
-                print(' ') 
                 index += 1
                 args_names = line.split('\t')
             else:
@@ -64,18 +101,21 @@ def load_data(path):
                         x = '0'+x
                     instance.args.append(float(x))
                 instance.className = className
-                instance.args_names = args_names
+
                 index += 1
                 instances.append(instance)
 
+    return Instances(f.name,args_names,comments,instances)
 
 #read filepath from command prompt with no spaces
-#if len(sys.argv) > 1:
-#    filePath = sys.argv[1]
+if len(sys.argv) > 1:
+    filePath = sys.argv[1]
 
-#if not os.path.exists(filePath):
-#     raise Exception('File does not exists')
-filePath = "A:/IRISDAT.TXT"
-load_data(filePath)
-for instance in instances:
-    print(instance.toString())
+if not os.path.exists(filePath):
+     raise Exception('File does not exists')
+
+#filePath = "A:/IRISDAT.TXT"
+io = load_data(filePath)
+print(io.toString())
+
+io.showInstances()
